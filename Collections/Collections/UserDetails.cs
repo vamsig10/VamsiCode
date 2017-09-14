450538f0b40be00109be6051b5aed9053278ebfa
Vamsi Gamidi <vamsi.gamidi@aveva.com>
2017-09-14 11:02:39 +0530

    Added Collections

---
+++ Collections/Collections/UserDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Collections
+{
+    public class UserDetails
+    {
+        public int UserId { get; set; }
+        public string Fname { get; set; }
+        public string Lname { get; set; }      
+        public long PhoneNo { get; set; }
+
+    }
+}
