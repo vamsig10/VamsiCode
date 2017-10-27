3b72b3501ef36f1aca018fff897594661e625725
Vamsi Gamidi <vamsi.gamidi@aveva.com>
2017-10-27 17:19:37 +0530

    Added DependencyProperties2

---
+++ DependencyProperties2/DependencyProperties/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DependencyProperties
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            //FullName = "Vamsi Gamidi";
+            
+
+        }
+        public static readonly DependencyProperty FullNameProperty=DependencyProperty.Register("FullName",typeof(string),typeof(MainWindow),new PropertyMetadata("",new PropertyChangedCallback(OnNameChanged)));
+
+        public string FullName
+        {
+            get => (string)GetValue(FullNameProperty);
+            set => SetValue(FullNameProperty,value);
+        }
+
+        private static void OnNameChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            // MessageBox.Show("Name Changed");
+         
+        }
+
+       
+    }
+}
